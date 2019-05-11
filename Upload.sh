67f9cfdfafd1a35e07f488d47beaa58ddd520549
unknown <waldemar.hoffman@arcor.de>
2019-05-11 18:52:56 +0200

    made changes

--- Upload.sh
+++ Upload.sh
@@ -1,5 +1,6 @@
-#!/bin/bash  
-git add .  
-read -p "Commit description: " desc  
-git commit -m $desc  
-git push origin master
+#!/bin/sh
+cd /c/github/frx
+git checkout dev
+git add C:\Users\WH\AppData\Roaming\MetaQuotes\Terminal\2E8DC23981084565FA3E19C061F586B2\MQL4\Files\marcetdata.html
+git commit -am "made changes"
+git push
