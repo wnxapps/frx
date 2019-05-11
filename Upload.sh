777598f1b57250050bb737e1e681146b66035654
unknown <waldemar.hoffman@arcor.de>
2019-05-11 19:27:30 +0200

    made changes

--- Upload.sh
+++ Upload.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd /c/github/frx
 git checkout dev
-git add C:\Users\WH\AppData\Roaming\MetaQuotes\Terminal\2E8DC23981084565FA3E19C061F586B2\MQL4\Files\marcetdata.html
+git add marcetdata.html
 git commit -am "made changes"
 git push
